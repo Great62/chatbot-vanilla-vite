6aa535a9a66bf77a66d1e029c9b6c6d0617d842d
Adam <Adamkindel1234@gmail.com>
2023-08-19 10:04:18 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+import { styles } from './asset.js';
+
+class ChatBotWidget {
+  constructor() {
+    this.initialize();
+    this.injectStyles();
+  }
+
+  async initialize() {
+    document.addEventListener('DOMContentLoaded', function () {
+      const createHTMLElement = (tagName, attributes = {}, content = '') => {
+        const element = document.createElement(tagName);
+        for (const [attr, value] of Object.entries(attributes)) {
+          element.setAttribute(attr, value);
+        }
+        element.innerHTML = content;
+        return element;
+      };
+    
+      const chatbotToggler = createHTMLElement('button', { class: 'chatbot-toggler' }, `
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+          </svg>        
+        </span>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-10 h-10">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </span>
+      `);
+    
+      const chatbot = createHTMLElement('div', { class: 'chatbot' }, `
+        <header>
+          <h2>Assistance</h2>
+          <span class="close-btn material-symbols-outlined">close</span>
+        </header>
+        <ul class="chatbox">
+          
+        </ul>
+        <div class="chat-input">
+          <textarea placeholder="Enter a message..." spellcheck="false" required></textarea>
+          <span id="send-btn" class="material-symbols-rounded">send</span>
+        </div>
+      `);
+    
+      document.body.appendChild(chatbotToggler);
+      document.body.appendChild(chatbot);
+    });
+  }
+
+  injectStyles() {
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = styles.replace(/^\s+|\n/gm, "");
+    document.head.appendChild(styleTag);
+  }
+}
+
+const initializeChatbot = () => {
+  new ChatBotWidget();
+}
+
+initializeChatbot();
+
+document.addEventListener('DOMContentLoaded', function () {
+  const script = document.createElement('script');
+  script.src = 'script.js';
+  script.defer = true; // Optionally, set defer or async attribute
+  document.body.appendChild(script);
+});
---
+++ script.js
@@ -0,0 +1,134 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+const welcomeMessage = 'Hi there 👋! I\'m your friendly assistant here to help you. Whether you have questions, need assistance, or just want to chat, I\'m here for you. Feel free to type in your message'
+const welcomeQuestion = 'How can I help you?'
+let userMessage = null; // Variable to store user's message
+let messages = [{ id: 0, speaker: 'salesman', text: welcomeMessage }, { id: 1, speaker: 'salesman', text: welcomeQuestion }];  // Variable to store all the messages including the starter message
+const inputInitHeight = chatInput.scrollHeight;
+const productName = 'jarvis standing desk';
+const appId = '5f9b3b5b-8b0a-4b0a-8b0a-4b0a8b0a4b0a';
+
+const createChatLi = (message, className, pictureHidden = false) => {
+  // Create a chat <li> element with passed message and className
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+  let chatContent = className === "outgoing" ? `<p></p>` : `<span class="bot-picture-container ${pictureHidden ? 'hidden' : ''}"><img class='bot-picture' src='/icon-bot-96.png'/></span><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi; // return chat <li> element
+}
+
+const init = () => {
+  // get messages from local storage
+  const messagesFromLocalStorage = JSON.parse(localStorage.getItem('messages'));
+  if (messagesFromLocalStorage && messagesFromLocalStorage.length > 0) {
+    messages = messagesFromLocalStorage;
+    messages.forEach((message, index) => {
+      const hidePicture = index + 1 !== messages.length && messages[index + 1]?.speaker === message.speaker;
+      const chatElement = createChatLi(message.text, message.speaker === 'user' ? 'outgoing' : 'incoming', hidePicture);
+      chatbox.appendChild(chatElement);
+    });
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+  } else {
+    const chatElement = createChatLi(welcomeMessage, "incoming", true);
+    chatbox.appendChild(chatElement);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    const chatElement2 = createChatLi(welcomeQuestion, "incoming");
+    chatbox.appendChild(chatElement2);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+  }
+}
+
+init();
+
+const generateResponse = async (chatElement) => {
+  if (userMessage.trim() === '') return;
+  const messageElement = chatElement.querySelector("p");
+  messages = [...messages, {id: messages.length, speaker: 'user', text: userMessage}]
+  console.log('submitting message...');
+  console.log('usermessage: ', userMessage);
+  console.log('messages: ', messages);
+
+  try {
+    // // User is logged in, get the Firebase ID token
+    // const idToken = await auth.currentUser?.getIdToken();
+
+    // call cloud function
+    const response = await fetch(
+      `https://us-central1-sales-chatbot-f1521.cloudfunctions.net/openAISalesConversation`,
+      {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${''}`, // idToken
+        },
+        body: JSON.stringify({ messages: messages, productName: productName, namespace: `app-${appId}` })
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    if (data?.response?.replace('[salesman]:', '')?.includes('|||')) {
+      const split = data?.response?.replace('[salesman]:', '')?.split('|||')
+      messages = [...messages, {id: messages.length, speaker: 'salesman', text: split[0].trim()}, {id: messages.length + 1, speaker: 'salesman', text: split[1].trim().replace('Question: ', '')}]
+      chatElement.querySelector('span').classList.add("hidden");
+      messageElement.textContent = messages[messages.length - 2].text;
+      const incomingChatLi = createChatLi(messages[messages.length - 1].text, "incoming");
+      chatbox.appendChild(incomingChatLi);
+      chatbox.scrollTo(0, chatbox.scrollHeight);
+    } else {
+      messages = [...messages, {id: messages.length, speaker: 'salesman', text: data?.response?.replace('[salesman]:', '').trim()}]      
+    }
+
+    // update local storage
+    localStorage.setItem('messages', JSON.stringify(messages));
+
+    console.log(messages);
+  } catch (error) {
+    messageElement.classList.add("error");
+    messageElement.textContent = "Oops! Something went wrong. Please try again.";
+  }
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+    if(!userMessage) return;
+
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    
+    setTimeout(() => {
+        // Display "Thinking..." message while waiting for the response
+        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
