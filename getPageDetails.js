575e2d1af144cd63c45588da1161c9bc240a2743
Adam <Adamkindel1234@gmail.com>
2023-08-21 09:22:18 +0200

    fix: minor fix

--- getPageDetails.js
+++ getPageDetails.js
@@ -1,4 +1,4 @@
-const shopifyPageDetails = () => {
+export const shopifyPageDetails = () => {
   const pageType = window?.meta?.pageType;
   const productName = pageType === 'product' ? window?.meta?.product?.variants?.[0]?.name : null;
   return {
